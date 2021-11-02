42d42b6580fe31e2eae3b53751bfa48bd8a62908
ramelmueller <lukas.rammelmueller@gmail.com>
2021-11-02 13:51:32 +0100

    Implemented and checked effective interaction.

--- examples/harmonic_oscillator_1d/eff_relative.jl
+++ examples/harmonic_oscillator_1d/eff_relative.jl
@@ -9,9 +9,10 @@ using SpecialFunctions
 using SpecialPolynomials
 using Base.Cartesian
 using LinearAlgebra
-include("helper.jl")
+using Roots
+
 include("ho_typedefs.jl")
-include("../typedefs.jl")
+
 
 # Provides a numerically stable version of confluent hypergeometric functions
 # (and much more) that is required for large arguments at large principal quantum
@@ -19,6 +20,38 @@ include("../typedefs.jl")
 # and the integrals in the effective interaction construction are garbage.
 using PyCall
 py_mpm = pyimport("mpmath")
+
+
+function find_energy_solutions(N::Int, coupling::Float64, energy_equation::Function, epsilon=1e-12; init_seg::Float64=-0.5)
+    """ Finds the lowest N solutions for a given (possibly transcendental)
+        equation.
+
+        This probably works more broadly, however, is implemented for the HO
+        solutions. Considers that the gamma function is not defined at negative
+        integer values, by repeatedly constructing intervals for the root finder
+        that cut out the problematic points. The details are dependent on the
+        dimension and its behavior can be toggled via the optional keyword argument
+        init_seg, which defaults to 0.5 (value for 1D and 3D, although 3D was not
+        tested). For 2D the routine should be called with init_seg = 0.
+
+        Notes:
+        -   We assume there's at most one negative energy solution.
+        -   The epsilon parameter cuts out a neighborhood around the points where
+            the gamma points are not defined (i.e., negative integer values).
+            Results should be independent of these values for couplings that are
+            larger than epsilon - if (very unexpectedly) needed, this parameter
+            can be adapted.
+    """
+    evals = Vector{Float64}()
+    seg = min(-coupling^2/2, -5.5)
+    while length(evals) < N
+        new_upper = max(init_seg, seg) + 1
+        lower, upper = seg+epsilon, new_upper-epsilon
+        append!(evals, find_zeros(e -> energy_equation(e, coupling), lower, upper))
+        seg = new_upper
+    end
+    return evals
+end
 
 
 # ------------------------------------------------------------------------------
@@ -70,12 +103,11 @@ end
 # ------------------------------------------------------------------------------
 # Interaction matrix stuff.
 
-function construct_effective_interaction(::Type{HOOrbital1D}, n_basis::Integer, coupling::AbstractFloat; n_states::Union{IType,Nothing}=nothing)::InteractionMatrix
+function construct_effective_interaction(ho_orbital::HOOrbital1D, n_basis::Integer, coupling::AbstractFloat; n_states::Union{Integer,Nothing}=nothing)::InteractionMatrix
     """ Constructs the matrix for the effective interaction in 1D HO.
     """
     # Find energies for the interacting two-body problem.
     e_int = find_energy_solutions(n_basis, coupling, _energy_equation_ho1d)
-
 
     # Set the appropriate size to construct if not specified.
     if isnothing(n_states)
@@ -86,17 +118,17 @@ function construct_effective_interaction(::Type{HOOrbital1D}, n_basis::Integer, 
     e = fill(0.0, (n_states))
     U = fill(0.0, (n_states, n_states))
     for i = 1:n_states
-        orb = HOOrbital1D(i)
         if mod(i,2) == 0
             # We're solving the relative problem here -> need to remove the c.m. energy.
-            e[i] = orb.e
+            e[i] = ho_orbital(i)
         end
         for j = 1:n_states
             if (mod(i,2)==1) && (mod(j,2)==1)
                 iorb = InteractingHOOrbital1D(e_int[Int((j+1)/2)])
                 e[j] = iorb.e
+
                 # Watch the ordering: must be [j,i] in order to satisfy the equations.
-                U[j,i] = coupling * iorb(0) * orb(0) / (orb.e - iorb.e)
+                U[j,i] = coupling * iorb(0.0) * ho_orbital(i, 0.0) / (ho_orbital(i) - iorb.e)
                 # Note:
                 # There's a factor (-g) in the notes, but this is completely
                 # irrelevant here, since it is going to be normalized and
@@ -125,7 +157,7 @@ function construct_effective_interaction(::Type{HOOrbital1D}, n_basis::Integer, 
     Q = U*inv(sqrt(U'*U))
 
     # Perform the transformation to actually get the effective interaction.
-    T = Diagonal([HOOrbital1D(k).e for k=1:n_states])
+    T = Diagonal([ho_orbital(k) for k=1:n_states])
     # T = Diagonal([HOOrbital1D(k).e - (k+1)%2 for k=1:n_states])
     E = Diagonal(e)
     return (Q'*E)*Q - T
